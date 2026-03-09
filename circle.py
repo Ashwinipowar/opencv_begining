8903e4963806b3e4b1d9bf9d6a5adbe216f1ad71
ashwinii <ashwinipowar2111@gmail.com>
2026-03-09 22:09:34 +0530

    Add files via upload

---
+++ circle.py
@@ -0,0 +1,11 @@
+import cv2
+image=cv2.imread("python.png")
+if image is None:
+    print("image not loaded")
+else:
+    print("image loaded successsfully")
+    cv2.circle(image,(210,300),50,(0,0,255),-1)
+
+    cv2.imshow("drawed circle",image)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
