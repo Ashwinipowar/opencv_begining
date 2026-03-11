f515e19314fe1bb1dac0973ac09552b899f1abfe
ashwinii <ashwinipowar2111@gmail.com>
2026-03-11 17:03:00 +0530

    Add files via upload

---
+++ flipped.py
@@ -0,0 +1,16 @@
+import cv2
+image=cv2.imread("python.png")
+if image is None:
+    print("could not load image")
+else:
+    flipped_horizontal=cv2.flip(image,1)
+    flipped_vertical=cv2.flip(image,0)
+    flipped_both=cv2.flip(image,-1)
+
+    cv2.imshow("origonal",image)
+    cv2.imshow("flipped_horizontal",flipped_horizontal)
+    cv2.imshow("flipped_vertical",flipped_vertical)
+    cv2.imshow("both",flipped_both)
+
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
