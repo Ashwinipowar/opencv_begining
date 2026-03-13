ea9422a8554141afce7117e44a3679b7bb259dc9
ashwinii <ashwinipowar2111@gmail.com>
2026-03-13 17:48:00 +0530

    Update canny edge.py

--- canny edge.py
+++ canny edge.py
@@ -1,3 +1,4 @@
+#we use canny for the grayscale
 import cv2
 image=cv2.imread("grayscale.png")
 edge=cv2.Canny(image,50,150)
@@ -5,4 +6,5 @@ edge=cv2.Canny(image,50,150)
 cv2.imshow("original image",image)
 cv2.imshow("edged image",edge)
 cv2.waitKey(0)
+
 cv2.destroyAllWindows()
