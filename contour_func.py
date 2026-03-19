d6c61a63238a9377c760d90f91ed23ea4bb0bc95
ashwinii <ashwinipowar2111@gmail.com>
2026-03-19 19:41:12 +0530

    Refactor contour detection code with comments

--- contour_func.py
+++ contour_func.py
@@ -1,10 +1,12 @@
+#here we gave the trianle image
+#this o/p will show the border on the trianlge
 import cv2
 img=cv2.imread("image Resizing and Shaping\Edge detection an thresholding\image.png")
 gray=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 _,thresh=cv2.threshold(gray,210,255,cv2.THRESH_BINARY)
 
 #find contours
-#this underscore for variable which will we nwwd in future
+#this underscore for variable which will we in future
 
 contours , heirachy =cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
 
